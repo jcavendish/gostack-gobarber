2255507553e572754cb1154fbee1cb60c1da3b8d
Joao Paulo Cavendish <joaopaulocavendish@gmail.com>
2020-04-29 18:41:53 -0300

    add express async errors

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify, decode } from 'jsonwebtoken';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 interface TokenPayload {
   iat: number;
@@ -12,11 +13,11 @@ export default function ensureAuthenticated(
   request: Request,
   response: Response,
   next: NextFunction
-) {
+): void {
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw Error('The JWT is missing');
+    throw new AppError('The JWT is missing', 401);
   }
 
   const [, token] = authHeader.split(' ');
@@ -30,6 +31,6 @@ export default function ensureAuthenticated(
     };
     next();
   } catch {
-    throw new Error('Invalid JWT token');
+    throw new AppError('Invalid JWT token', 401);
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import cors from 'cors';
 import routes from './routes';
 
